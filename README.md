7f376f8a0bdf3fb95bcca88b7a32ab14da9c5d7f
popstas <popstas@gmail.com>
2015-04-14 09:33:44 +0500

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ansible-ubuntu-desktop
+
+# Install
+wget -O- https://raw.githubusercontent.com/popstas/ansible-ubuntu-desktop/master/setup.sh | sh
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo apt-get install -y git python-pip python-dev
+sudo pip install ansible
+git clone https://github.com/popstas/ansible-ubuntu-desktop.git
+cd ansible-ubuntu-desktop
+ansible-playbook install.yml -i hosts install.yml
